9ed6cf34c9e2bd98f137fe7a6be1af2b03b26d6b
AndrewIlkiv <ekomanan@gmail.com>
2020-03-10 23:59:56 +0200

    Added goal table and created its migrations

---
+++ PerfectDay/Entities/Goal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerfectDay.Entities
+{
+    public class Goal
+    {
+        public int Id { get; set; }
+        public String Description { get; set; }
+        public int Frequency { get; set; }
+    }
+}
