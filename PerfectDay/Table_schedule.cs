5deea8e9068cccb71d046d9357595ec03f0d9056
vlad.kondratskyi <vladkondratskyi@gmail.com>
2020-03-12 21:34:37 +0200

    TableSchedule

---
+++ PerfectDay/Table_schedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PerfectDay
+{
+    public class Schedule
+    {
+        public int Schedule_Id { get; set; }
+        public int User_Id { get; set; }
+        public TimeSpan availableTime { get; set; }
+        public char taskSet { get; set; }
+        public DateTime day { get; set; }
+    }
+}
