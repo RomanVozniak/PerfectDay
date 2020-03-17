7c39e539abd6b72d726f8947e31a8ac90a4479eb
s6nch0s <sashka.vipka@gmail.com>
2020-03-17 21:45:30 +0200

    Adding extra files

---
+++ PerfectDay/Entities/ApplicationContex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PerfectDay.Entities
+{
+    public class ApplicationContex : DbContext
+    {
+        public DbSet<Task> Task { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=prefrectdaydb;Trusted_Connection=True;");
+        }
+    }
+}
