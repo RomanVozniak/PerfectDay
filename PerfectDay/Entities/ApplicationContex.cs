c8643ad390613c07873a53a2f817f454a80e1dfe
kachmaryk <vitya.kachmaryk@gmail.com>
2020-03-17 22:14:40 +0200

    4th try))

--- PerfectDay/Entities/ApplicationContex.cs
+++ PerfectDay/Entities/ApplicationContex.cs
@@ -8,8 +8,6 @@ namespace PerfectDay.Entities
 {
     public class ApplicationContex : DbContext
     {
-        public DbSet<User> Goals { get; set; }
-
         public DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
