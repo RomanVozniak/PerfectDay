442c0c0422763b5a655bfea470564ed393f9ebfc
AndrewIlkiv <ekomanan@gmail.com>
2020-03-17 21:23:57 +0200

    After merge fixes

--- PerfectDay/Entities/ApplicationContex.cs
+++ PerfectDay/Entities/ApplicationContex.cs
@@ -8,12 +8,9 @@ namespace PerfectDay.Entities
 {
     public class ApplicationContex : DbContext
     {
-<<<<<<< HEAD
-        public DbSet<Goal> goals { get; set; }
         public DbSet<User> users { get; set; }
-=======
+
         public DbSet<Goal> Goals { get; set; }
->>>>>>> f6a89a83ea0b18c5b991e44fd966537ac214a12f
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
