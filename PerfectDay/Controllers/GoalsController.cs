347b148a45f46ce4385e01e33cfcc1517e9cd38e
AndrewIlkiv <60966071+AndrewIlkiv@users.noreply.github.com>
2020-03-22 18:55:15 +0200

    Created Controller Class for Goal

---
+++ PerfectDay/Controllers/GoalsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PerfectDay.Entities;
+
+namespace PerfectDay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalsController : ControllerBase
+    {
+        private readonly ApplicationContex _context;
+
+        public GoalsController(ApplicationContex context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Goals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Goal>>> GetGoals()
+        {
+            return await _context.Goals.ToListAsync();
+        }
+
+        // GET: api/Goals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Goal>> GetGoal(int id)
+        {
+            var goal = await _context.Goals.FindAsync(id);
+
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            return goal;
+        }
+
+        // PUT: api/Goals/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGoal(int id, Goal goal)
+        {
+            if (id != goal.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(goal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GoalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Goals
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Goal>> PostGoal(Goal goal)
+        {
+            _context.Goals.Add(goal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGoal", new { id = goal.Id }, goal);
+        }
+
+        // DELETE: api/Goals/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Goal>> DeleteGoal(int id)
+        {
+            var goal = await _context.Goals.FindAsync(id);
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Goals.Remove(goal);
+            await _context.SaveChangesAsync();
+
+            return goal;
+        }
+
+        private bool GoalExists(int id)
+        {
+            return _context.Goals.Any(e => e.Id == id);
+        }
+    }
+}
