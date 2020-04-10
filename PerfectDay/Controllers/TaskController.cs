11f33b562e153ace7e53a02715fcac27d49cef8f
s6nch0s <sashka.vipka@gmail.com>
2020-04-10 22:42:48 +0300

    finish with repositories

---
+++ PerfectDay/Controllers/TaskController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PerfectDay.Repositories;
+using PerfectDay.Entities;
+
+namespace PerfectDay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ApplicationContex _context;
+        [HttpGet]
+        public async Task<>()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ PerfectDay/Repositories/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PerfectDay.Entities;
+
+namespace PerfectDay.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly ApplicationContex _context;
+
+        public BaseRepository(ApplicationContex context)
+        {
+            _context = context;
+        }
+    }
+}
