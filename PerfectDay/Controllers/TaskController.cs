f7dec985cfa4c2981008b14ca54cdfd987a91b80
s6nch0s <sashka.vipka@gmail.com>
2020-04-11 22:33:16 +0300

    created a TaskController

--- PerfectDay/Controllers/TaskController.cs
+++ PerfectDay/Controllers/TaskController.cs
@@ -5,19 +5,69 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PerfectDay.Repositories;
 using PerfectDay.Entities;
+using AutoMapper;
 
 namespace PerfectDay.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
     public class TaskController : ControllerBase
     {
-        private readonly ApplicationContex _context;
-        [HttpGet]
-        public async Task<>()
-        {
+        private readonly IRepositoryTask<Entities.Task> _repositoryTask;
+        private readonly IMapper _mapper;
 
-            return View();
+        public TaskController(IRepositoryTask<Entities.Task> repositoryTask, IMapper mapper)
+        {
+            _repositoryTask = repositoryTask;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IEnumerable<Entities.Task> Get()
+        {
+            return _repositoryTask.GetAll();
+        }
+        [Route("/api/tasks/get/{id}")]
+        [HttpGet]
+        public  IActionResult Get(int id)
+        {
+            Entities.Task task = _repositoryTask.FindById(id);
+            if (id <= 0) return BadRequest("Error");
+            return (IActionResult) task;
+        }
+        [Route("/api/{controller}/add")]
+        [HttpPost]
+        public IActionResult Add(Entities.Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Error");
+            }
+            else {
+                _repositoryTask.Create(task);
+            return Ok();
+            }
+        }
+        [Route("/api/{controller}/update")]
+        [HttpPost]
+        public IActionResult Update(Entities.Task task)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Error");
+            }
+            else
+            {
+                _repositoryTask.Update(task);
+                return Ok();
+            }
+        }
+        [Route("/api/{controller}/delete/{id}")]
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Entities.Task task = _repositoryTask.FindById(id); 
+            if (id <= 0) return BadRequest("Error");
+            _repositoryTask.Delete(task);
+            return Ok();
         }
     }
 }
