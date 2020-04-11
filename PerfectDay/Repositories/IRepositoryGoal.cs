09619a55c5d70dcd4661a611ffaeb0c56f702b46
AndrewIlkiv <60966071+AndrewIlkiv@users.noreply.github.com>
2020-04-11 20:28:58 +0300

    Changed repository

--- PerfectDay/Repositories/IRepositoryGoal.cs
+++ PerfectDay/Repositories/IRepositoryGoal.cs
@@ -1,48 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using PerfectDay.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PerfectDay.Repositories
 {
-    public class IRepositoryGoal<TEntity> : IRepository<TEntity> where TEntity : class
+    public interface IRepositoryGoal<TEntity>
     {
-        ApplicationContex _context;
-        DbSet<TEntity> _dbSet;
-        public IRepositoryGoal(ApplicationContex context)
-        {
-            _context = context;
-            _dbSet = context.Set<TEntity>();
-        }
-        public void Create(TEntity item)
-        {
-            _dbSet.Add(item);
-            _context.SaveChanges();
-        }
-
-        public void Delete(TEntity item)
-        {
-            _dbSet.Remove(item);
-            _context.SaveChanges();
-
-        }
-
-        public TEntity FindById(int id)
-        {
-            return _dbSet.Find(id);
-        }
-
-        public void Update(TEntity item)
-        {
-            _context.Entry(item).State = EntityState.Modified;
-            _context.SaveChanges();
-        }
-
-        public IEnumerable<TEntity> GetAll(TEntity item)
-        {
-            return _context.Set<TEntity>().ToList();
-        }
+        void Create(TEntity item);
+        void Update(TEntity item);
+        TEntity FindById(int id);
+        void Delete(TEntity item);
+        IEnumerable<TEntity> GetAll(TEntity item);
     }
 }
