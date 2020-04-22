fa0dc03cbb9147267bf4e86507b7ccfe8ebc06f2
Viktor Kachmaryk <vitya.kachmaryk@gmail.com>
2020-04-22 22:28:29 +0300

    Fixed condilicts

--- PerfectDay/Repositories/IRepositoryUser.cs
+++ PerfectDay/Repositories/IRepositoryUser.cs
@@ -5,17 +5,12 @@ using System.Threading.Tasks;
 
 namespace PerfectDay.Repositories
 {
-<<<<<<< HEAD:PerfectDay/Repositories/IRepositoryUser.cs
     public interface IRepositoryUser<TEntity>
-=======
-    public interface IRepositoryTask<TEntity>
->>>>>>> sprint_4:PerfectDay/Repositories/IRepositoryTask.cs
     {
         void Create(TEntity item);
         void Update(TEntity item);
         TEntity FindById(int id);
         IEnumerable<TEntity> GetAll();
         void Delete(TEntity item);
-        IEnumerable<TEntity> GetAll();
     }
 }
