1bb3c3f3d0ec27ef8caae6acf610a59feef7c8db
SofiaHolosko <sofia.golosko@gmail.com>
2020-04-24 21:15:29 +0300

    Added GoalId field

--- PerfectDay/Entities/History.cs
+++ PerfectDay/Entities/History.cs
@@ -9,6 +9,7 @@ namespace PerfectDay.Entities
     {   
         [Key]
         public int Id { get; set; }
+        public int GoalId{ get; set;}
         public DateTime updateTime { get; set; }
         [MaxLength(255)]
         public string Description { get; set; }
